c636cb90d29036d0508cbbdab3d44f1aad97e288
Tiffany Boorsma <tiffanyboorsma@gmail.com>
2018-08-05 13:53:35 -0400

    Add react-router-dom and use init in client app

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,33 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+const Recipes = () => (
+  <div>
+    <h2>Recipes</h2>
+  </div>
+)
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+
+          <Route exact path="/" component={ Recipes }/>
+        </div>
+      </Router>
+    )
   }
 }
 
 export default App;
+
